e0a476f5dcd88e561875449345f87488278c2b3a
Mateusz Goślinowski <mateusz.goslinowski@gmail.com>
2021-09-10 11:20:46 +0200

    Move utils implementation to header, allowing for templating out double parameter

--- utils.hpp
+++ utils.hpp
@@ -7,6 +7,7 @@
 
 #include <vector>
 #include <string>
+#include <execution>
 
 using std::vector;
 
@@ -16,7 +17,10 @@ using std::vector;
  * @param v second vector
  * @return u * v
  */
-double dot(const vector<double>& u, const vector<double>& v);
+template<typename T>
+double dot(const vector<T>& u, const vector<T>& v) {
+    return std::transform_reduce(std::execution::par_unseq, u.begin(), u.end(), v.begin(), 0.0);
+}
 
 /**
  * Compute a squared distance between two vectors.
@@ -24,7 +28,10 @@ double dot(const vector<double>& u, const vector<double>& v);
  * @param v second vector
  * @return |u-v|^2
  */
-double dist_sq(const vector<double>& u, const vector<double>& v);
+template <typename T>
+double dist_sq(const vector<T>& u, const vector<T>& v) {
+    return dot(u-v, u-v);
+}
 
 /**
  * Compute component-wise sum of two vectors
@@ -32,7 +39,13 @@ double dist_sq(const vector<double>& u, const vector<double>& v);
  * @param v second vector
  * @return u + v
  */
-vector<double> operator+(const vector<double>& u, const vector<double>& v);
+template <typename T>
+vector<T> operator+(const vector<T>& u, const vector<T>& v) {
+    vector<T> res(u.size());
+    std::transform(std::execution::par_unseq, u.begin(), u.end(), v.begin(), res.begin(), std::plus<>());
+
+    return res;
+}
 
 /**
  * Compute component-wise difference of two vectors
@@ -40,7 +53,13 @@ vector<double> operator+(const vector<double>& u, const vector<double>& v);
  * @param v second vector
  * @return u - v
  */
-vector<double> operator-(const vector<double>& u, const vector<double>& v);
+template <typename T>
+vector<T> operator-(const vector<T>& u, const vector<T>& v) {
+    vector<T> res(u.size());
+    std::transform(std::execution::par_unseq, u.begin(), u.end(), v.begin(), res.begin(), std::minus<>());
+
+    return res;
+}
 
 /**
  * Multiply vector by a matrix (from the left)
@@ -48,14 +67,33 @@ vector<double> operator-(const vector<double>& u, const vector<double>& v);
  * @param v vector factor
  * @return w * v
  */
-vector<double> operator*(const vector<vector<double>>& w, const vector<double>& v);
+template <typename T>
+vector<T> operator*(const vector<vector<T>>& w, const vector<T>& v) {
+    vector<T> res(w.size());
+
+    std::transform(std::execution::par_unseq, w.begin(), w.end(), res.begin(), [&](auto& wv) {
+        return std::transform_reduce(std::execution::par_unseq, wv.begin(), wv.end(), v.begin(), 0.0);
+    });
+
+    return res;
+}
 
 /**
  * Transpose a matrix
  * @param matrix
  * @return transposed matrix
  */
-std::vector<std::vector<double>> transpose (const std::vector<std::vector<double>>& matrix);
+template <typename T>
+std::vector<std::vector<T>> transpose (const std::vector<std::vector<T>>& matrix) {
+    std::vector<std::vector<T>> res (matrix.front().size());
+    for(int i=0; i<res.size(); ++i) {
+        res[i].resize(matrix.size());
+        for(int j=0; j<matrix.size(); ++j)
+            res[i][j] = matrix[j][i];
+    }
+
+    return res;
+}
 
 /**
  * Multiply a vector by a scalar
@@ -63,7 +101,14 @@ std::vector<std::vector<double>> transpose (const std::vector<std::vector<double
  * @param v a vector
  * @return xv
  */
-vector<double> operator*(double x, const vector<double>& v);
+template <typename T>
+vector<T> operator*(T x, const vector<T>& v) {
+    using std::placeholders::_1;
+    vector<T> res(v.size());
+
+    std::transform(std::execution::par_unseq, v.begin(), v.end(), res.begin(), std::bind(std::multiplies<>(), _1, x));
+    return res;
+}
 
 /**
  * Divide a vector by a scalar
@@ -71,26 +116,39 @@ vector<double> operator*(double x, const vector<double>& v);
  * @param x a scalar
  * @return v/x
  */
-vector<double> operator/(const vector<double>& v, double x);
+template <typename T>
+vector<T> operator/(const vector<T>& v, T x) {
+    using std::placeholders::_1;
+    vector<T> res(v.size());
+
+    std::transform(std::execution::par_unseq, v.begin(), v.end(), res.begin(), std::bind(std::divides<>(), _1, x));
+    return res;
+}
 
 /**
  * Compute a mean of a vector of vectors
  * @param xs set of vectors
  * @return a mean vector
  */
-vector<double> mean(const vector<vector<double>>& xs);
-
-/**
- * Prints the matrix to the string.
- * @param w matrix to be printed
- */
-std::string print_matrix(const vector<vector<double>>& w);
+template <typename T>
+vector<T> mean(const vector<vector<T>>& xs) {
+    vector<T> zero(xs.front().size(), 0.0);
+    return std::accumulate(xs.begin(), xs.end(), zero, [](auto&& x, auto&& y) {
+        return x+y;
+    }) / (T) xs.size();
+}
 
 /**
  * Calculate a size of a matrix (a number of entries)
  * @param w A matrix to measure
  * @return A number of entries in the matrix;
  */
-int matrix_size(const vector<vector<double>>& w);
+template <typename T>
+int matrix_size(const vector<vector<T>>& w) {
+    return std::transform_reduce(std::execution::par_unseq,
+                                 w.begin(), w.end(), 0,
+                                 std::plus<>(), std::mem_fn(&vector<T>::size));
+}
+
 
 #endif //ITO_UTILS_HPP
